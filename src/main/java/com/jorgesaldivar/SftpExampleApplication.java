89cbea9e3db4b1273fbb4baf0db9b19fafb73d1d
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2016-10-18 23:55:16 -0500

    commit

---
+++ src/main/java/com/jorgesaldivar/SftpExampleApplication.java
@@ -0,0 +1,12 @@
+package com.jorgesaldivar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftpExampleApplication {
+
+	public static void main(String[] args) { 
+		SpringApplication.run(SftpExampleApplication.class, args);
+	}
+}

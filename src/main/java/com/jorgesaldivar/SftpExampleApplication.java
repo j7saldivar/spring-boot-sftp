8207688028e396fab36696fde3e26cafc84290cc
Jorge Saldivar <1724724+jorgesaldivar@users.noreply.github.com>
2016-10-19 00:00:08 -0500

    example-setup

--- src/main/java/com/jorgesaldivar/SftpExampleApplication.java
+++ src/main/java/com/jorgesaldivar/SftpExampleApplication.java
@@ -1,12 +1,152 @@
 package com.jorgesaldivar;
 
-import org.springframework.boot.SpringApplication;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.expression.common.LiteralExpression;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.file.FileNameGenerator;
+import org.springframework.integration.file.remote.session.CachingSessionFactory;
+import org.springframework.integration.file.remote.session.SessionFactory;
+import org.springframework.integration.sftp.outbound.SftpMessageHandler;
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+
+import com.jcraft.jsch.ChannelSftp.LsEntry;
 
 @SpringBootApplication
+@IntegrationComponentScan
 public class SftpExampleApplication {
 
-	public static void main(String[] args) { 
-		SpringApplication.run(SftpExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+                    new SpringApplicationBuilder(SftpExampleApplication.class)
+                        .web(false)
+                        .run(args);
+        MyGateway gateway = context.getBean(MyGateway.class);
+        gateway.sendToSftp(new File("text.txt"));
+        context.close();
+    }
+
+    @Bean
+    public SessionFactory<LsEntry> sftpSessionFactory() {
+        DefaultSftpSessionFactory factory = new DefaultSftpSessionFactory(true);
+        factory.setHost("");
+        factory.setPort(22);
+        factory.setUser("");
+        factory.setPassword("");
+        factory.setAllowUnknownKeys(true);
+        factory.setPrivateKey(new Resource() {
+			
+			@Override
+			public InputStream getInputStream() throws IOException {
+				// TODO Auto-generated method stub
+				return new FileInputStream("");
+			}
+			
+			@Override
+			public long lastModified() throws IOException {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+			
+			@Override
+			public boolean isReadable() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public boolean isOpen() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public URL getURL() throws IOException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public URI getURI() throws IOException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public String getFilename() {
+				// TODO Auto-generated method stub
+				return "";
+			}
+			
+			@Override
+			public File getFile() throws IOException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public String getDescription() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public boolean exists() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+			
+			@Override
+			public Resource createRelative(String relativePath) throws IOException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public long contentLength() throws IOException {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+		});;
+        return new CachingSessionFactory<LsEntry>(factory);
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "toSftpChannel")
+    public MessageHandler handler() {
+        SftpMessageHandler handler = new SftpMessageHandler(sftpSessionFactory());
+        handler.setRemoteDirectoryExpression(new LiteralExpression("path-sent"));
+        handler.setFileNameGenerator(new FileNameGenerator() {
+
+            @Override
+            public String generateFileName(Message<?> message) {
+                 return "handlerContent.test";
+            }
+
+        });
+        return handler;
+    }
+
+    @MessagingGateway
+    public interface MyGateway {
+
+         @Gateway(requestChannel = "toSftpChannel")
+         void sendToSftp(File file);
+
+    }
 }
